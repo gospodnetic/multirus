f6067cf3b68a0f4fbabc2432165d1d5f871e81d4
gospodnetic <petra.gospodnetic@itwm.fraunhofer.de>
2022-06-17 17:24:54 +0200

    Read jpg, export lzw compressed tiff

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use image::{GenericImageView};
+use std::path::Path;
+use std::fs::File;
+use tiff::encoder::{TiffEncoder, colortype};
+use tiff::encoder::compression;
+
+fn main() {
+    let img = image::open("bess-axa.tif").unwrap();
+    println!("dimensions {:?}", img.dimensions());
+    println!("{:?}", img.color());
+
+    let dims = img.dimensions();
+
+    let output_filename = String::from("test");
+    let output = &mut File::create(&Path::new(&format!("{}.tif", output_filename))).unwrap();
+
+    let mut bigtiff = TiffEncoder::new_big(output).unwrap();
+    // bigtiff.write_image_with_compression::<colortype::RGB8, compression::Lzw>(dims.0, dims.1, compression::Lzw, &img.as_bytes()).unwrap();
+    bigtiff.write_image_with_compression::<colortype::RGB8, compression::Deflate>(dims.0, dims.1, compression::Deflate::default(), &img.as_bytes()).unwrap();
+}

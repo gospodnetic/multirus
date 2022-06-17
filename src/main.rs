b3236873bd17516f8478b21bfce94fb1bd3e0196
gospodnetic <petra.gospodnetic@itwm.fraunhofer.de>
2022-06-17 17:30:04 +0200

    Test with biggest possible compression

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ use tiff::encoder::{TiffEncoder, colortype};
 use tiff::encoder::compression;
 
 fn main() {
-    let img = image::open("bess-axa.tif").unwrap();
+    let img = image::open("bess-axa.jpg").unwrap();
     println!("dimensions {:?}", img.dimensions());
     println!("{:?}", img.color());
 
     let dims = img.dimensions();
 
-    let output_filename = String::from("test");
+    let output_filename = String::from("test_deflate_best");
     let output = &mut File::create(&Path::new(&format!("{}.tif", output_filename))).unwrap();
 
     let mut bigtiff = TiffEncoder::new_big(output).unwrap();
     // bigtiff.write_image_with_compression::<colortype::RGB8, compression::Lzw>(dims.0, dims.1, compression::Lzw, &img.as_bytes()).unwrap();
-    bigtiff.write_image_with_compression::<colortype::RGB8, compression::Deflate>(dims.0, dims.1, compression::Deflate::default(), &img.as_bytes()).unwrap();
+    bigtiff.write_image_with_compression::<colortype::RGB8, compression::Deflate>(dims.0, dims.1, compression::Deflate::with_level(compression::DeflateLevel::Best), &img.as_bytes()).unwrap();
 }
